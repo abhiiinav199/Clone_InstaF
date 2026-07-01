1e0613a1772e4662e5241d551da5dfbc7154cecb
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-07-02 00:17:49 +0530

    wip-added login in user.route.js and api working properly

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { otpSave, signUp } from "../controllers/auth.js"
+import { login, otpSave, signUp } from "../controllers/auth.js"
 
 const userRouter= express.Router()
 
 userRouter.post("/otp-create", otpSave)
 userRouter.post("/sign-up", signUp)
+userRouter.post("/log-in", login)
 
 export default userRouter
