c5e95588f82d692c07901f7245a38a13b760308b
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-07-03 18:41:26 +0530

    wip-made 2 api resetPassword and resetPasswordOtpVerify in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,12 +7,12 @@ import { generateToken } from "../utils/generateToken.js"
 
 
 //save otp in db and used pre method in OtpModel to send mail before saving data in db 
-export const otpSave= async(req, res) =>{
+export const otpSave = async (req, res) => {
     try {
         // fetch email
-        const {email} = req.body
+        const { email } = req.body
 
-        if(!email){
+        if (!email) {
             return res.status(400).json({
                 error: true,
                 success: false,
@@ -20,12 +20,12 @@ export const otpSave= async(req, res) =>{
             })
         }
         // find user is already registered
-        const userExists = await UserModel.findOne({email: email})
+        const userExists = await UserModel.findOne({ email: email })
 
-        if(userExists){
+        if (userExists) {
             return res.status(400).json({
-                error : true,
-                success:false,
+                error: true,
+                success: false,
                 message: "User is already registered"
             })
         }
@@ -36,17 +36,17 @@ export const otpSave= async(req, res) =>{
         })
 
         //create entry in Db
-        const newOtp = await OtpModel.create({email: email, otp:otp})
+        const newOtp = await OtpModel.create({ email: email, otp: otp })
 
         return res.status(200).json({
-            error:false,
-            success:true,
+            error: false,
+            success: true,
             message: "Otp sent successfully",
             otp: newOtp
         })
-        
+
     } catch (error) {
-        return res.status(500).json({ 
+        return res.status(500).json({
             message: error.message || error,
             error: true,
             success: false
@@ -54,12 +54,12 @@ export const otpSave= async(req, res) =>{
     }
 }
 
-export const signUp= async(req, res)=>{
+export const signUp = async (req, res) => {
     try {
-        const {userName, email, password, otp} = req.body
+        const { userName, email, password, otp } = req.body
 
         //validation
-        if(!userName || !email || !password || !otp){
+        if (!userName || !email || !password || !otp) {
             return res.status(400).json({
                 error: true,
                 success: false,
@@ -68,8 +68,8 @@ export const signUp= async(req, res)=>{
         }
 
         //check if user already exists or not
-        const existedUser =await UserModel.findOne({email: email})
-        if(existedUser){
+        const existedUser = await UserModel.findOne({ email: email })
+        if (existedUser) {
             return res.status(400).json({
                 error: true,
                 success: false,
@@ -77,39 +77,39 @@ export const signUp= async(req, res)=>{
             })
         }
 
-        const latestOtp = await OtpModel.findOne({email}).sort({createdAt: -1})
+        const latestOtp = await OtpModel.findOne({ email }).sort({ createdAt: -1 })
 
-        if(!latestOtp){
+        if (!latestOtp) {
             return res.status(400).json({
-                error: true, 
+                error: true,
                 success: false,
                 message: "OTP not found"
             })
         }
 
-        if(latestOtp.otp !== Number(otp)){
+        if (latestOtp.otp !== Number(otp)) {
             return res.status(400).json({
                 error: true,
                 success: false,
                 message: "OTP not matched"
             })
         }
-        
-        const hashedPassword= await hashingPassword(password)
+
+        const hashedPassword = await hashingPassword(password)
 
         const user = await UserModel.create({
-            userName, 
-            email, 
+            userName,
+            email,
             password: hashedPassword
         })
         return res.status(200).json({
-            error:false,
-            success:true,
+            error: false,
+            success: true,
             message: "User created successfully",
             user: user
         })
 
-        
+
     } catch (error) {
         return res.status(500).json({
             message: error.message || error,
@@ -120,12 +120,12 @@ export const signUp= async(req, res)=>{
 }
 
 
-export const login = async(req, res) =>{
+export const login = async (req, res) => {
     try {
-        const {email, password} = req.body
+        const { email, password } = req.body
 
         //validation
-        if(!email || !password){
+        if (!email || !password) {
             return res.status(400).json({
                 error: true,
                 success: false,
@@ -134,16 +134,16 @@ export const login = async(req, res) =>{
         }
 
         //check user account is created or not
-        const user = await UserModel.findOne({email:email}).select("+password")
+        const user = await UserModel.findOne({ email: email }).select("+password")
 
-        if(!user){
+        if (!user) {
             return res.status(400).json({
                 error: true,
                 success: false,
                 message: "Email is not registered"
             })
         }
-       const checkPassword = await bcryptjs.compare(password, user.password)
+        const checkPassword = await bcryptjs.compare(password, user.password)
         if (!checkPassword) {
             return res.status(400).json({
                 message: "Check your username & password",
@@ -151,37 +151,133 @@ export const login = async(req, res) =>{
                 success: false
             })
         }
-        
+
         //payload for jwt
-        const payload={
+        const payload = {
             userId: user._id,
-            userName: user.userName, 
-            email:user.email
+            userName: user.userName,
+            email: user.email
         }
         const token = await generateToken(payload)
 
-        const cookiesOptions= {
+        const cookiesOptions = {
             httpOnly: true,
-            secure:true,
+            secure: true,
             sameSite: "None"
         }
-        
+
         res.cookie('token', token, cookiesOptions)
 
         // while sending token or data you can also use this but always convert toObject() before setting data
-        const u= user.toObject()
-        u.token= token
-        u.password= undefined
+        const u = user.toObject()
+        u.token = token
+        u.password = undefined
         return res.status(200).json({
             message: "Login Successfully",
             error: false,
             success: true,
-            user:u
+            user: u
         })
 
-        
+
     } catch (error) {
         return res.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
+    }
+}
+
+//reset password form- fetch email, send otp
+export const resetPassword = async (req, res) => {
+    try {
+        // fetch email
+        const { email } = req.body
+
+        if (!email) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "Something went wrong during fetching email"
+            })
+        }
+        // find user is already registered
+        const userExists = await UserModel.findOne({ email: email })
+
+        if (!userExists) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "User is not registered"
+            })
+        }
+        const otp = otpgenerator.generate(4, {
+            upperCaseAlphabets: false,
+            lowerCaseAlphabets: false,
+            specialChars: false
+        })
+
+        //create entry in Db
+        const newOtp = await OtpModel.create({ email: email, otp: otp })
+
+        return res.status(200).json({
+            error: false,
+            success: true,
+            message: "Otp sent successfully",
+            otp: newOtp
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
+    }
+}
+
+//reset password otp verify
+export const resetPasswordOtpVerify = async (req, res) => {
+    try {
+        const { email, otp } = req.body
+
+        if (!email || !otp) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        //find latest otp with user eamil
+        const latestOtp = await OtpModel.findOne({ email }).sort({ createdAt: -1 })
+
+        if (!latestOtp) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "OTP not found"
+            })
+        }
+
+        if (latestOtp.otp !== Number(otp)) {
+            return res
+                .status(400).json({
+                    error: true,
+                    success: false,
+                    message: "OTP not matched"
+                })
+        }
+
+        return res.status(200).json({
+            error: false,
+            success: true,
+            message: "OTP verified successfully"
+        })
+
+    } catch (error) {
+        return res.status(400).json({
             message: error.message || error,
             error: true,
             success: false
