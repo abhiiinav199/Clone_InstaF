6026dec87028d2475a3db06310babea8cd3b418a
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-07-11 00:02:36 +0530

    wip- added acceptFollowRequest in followUnfollow.route.js

--- routes/followUnfollow.route.js
+++ routes/followUnfollow.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { loginValidation } from "../middlewares/Authorization.js"
-import { follow, unfollow } from "../controllers/followUnfollow.controler.js"
+import { acceptFollowRequest, follow, unfollow } from "../controllers/followUnfollow.controler.js"
 const followUnfollowRouter = express.Router()
 
 followUnfollowRouter.post("/follow", loginValidation, follow)
 followUnfollowRouter.delete("/unfollow", loginValidation, unfollow)
+followUnfollowRouter.post("/accept-follow-request", loginValidation, acceptFollowRequest)
 
 export default followUnfollowRouter
