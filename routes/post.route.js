2d70d618c4216bd392a606a6da46a688bffee676
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-15 23:21:31 +0530

    wip

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { loginValidation } from "../middlewares/Authorization.js"
-import { editPost, postUpload } from "../controllers/post.controller.js"
+import { deletePost, editPost, postUpload } from "../controllers/post.controller.js"
 import { upload } from "../middlewares/multer.js"
 
-const postRouter= express.Router()
+const postRouter = express.Router()
 
-postRouter.post("/post-upload",loginValidation, upload.array("post", 10) ,postUpload)
-postRouter.post("/edit-post",loginValidation, editPost)
+postRouter.post("/post-upload", loginValidation, upload.array("post", 10), postUpload)
+postRouter.post("/edit-post", loginValidation, editPost)
+postRouter.delete("/delte-post/:postId", loginValidation, deletePost)
 
 export default postRouter
