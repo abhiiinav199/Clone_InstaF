fb1d66b38525c85b0ab90a534bca0ec8a9aacaa6
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-15 21:50:37 +0530

    wip- added api editPost in post.route.js

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { loginValidation } from "../middlewares/Authorization.js"
-import { postUpload } from "../controllers/post.controller.js"
+import { editPost, postUpload } from "../controllers/post.controller.js"
 import { upload } from "../middlewares/multer.js"
 
 const postRouter= express.Router()
 
 postRouter.post("/post-upload",loginValidation, upload.array("post", 10) ,postUpload)
-
+postRouter.post("/edit-post",loginValidation, editPost)
 
 export default postRouter
