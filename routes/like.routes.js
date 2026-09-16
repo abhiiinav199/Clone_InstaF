ee32df3de72dabf8d441325dc19f8ff573886c69
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-16 21:02:47 +0530

    wip- added new unlikePost api in like.routes.js. Feature- Now officially unliking a post is rolled out for every user

--- routes/like.routes.js
+++ routes/like.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { likePost } from "../controllers/like.controller.js"
+import { likePost, unlikePost } from "../controllers/like.controller.js"
 import { loginValidation } from "../middlewares/Authorization.js"
 
 const likeRouter = express.Router()
 
 likeRouter.post("/like-post", loginValidation, likePost)
+likeRouter.delete("/unlike-post", loginValidation, unlikePost)
 
 export default likeRouter
