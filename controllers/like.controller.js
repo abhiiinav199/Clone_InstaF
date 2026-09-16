1ff2f2e62ac8cbdf7b2b8e2b71954b29969ded2d
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-16 20:58:49 +0530

    wip- made one api for unlike any  post(unlikeApi) in like.controller.js.

--- controllers/like.controller.js
+++ controllers/like.controller.js
@@ -19,8 +19,8 @@ export const likePost = async (req, res) => {
     // post exists or not and already liked or not using promises
     const [post, liked] = await Promise.all([
       PostModel.findById(postId),
-      LikeModel.findOne({ post: postId, user: userId })
-    ])
+      LikeModel.findOne({ post: postId, user: userId }),
+    ]);
     // const post = await PostModel.findById(postId); //this will search for post first then check if liked. Using promises we can do both at same time because these operations dont depend on each other and run in parallel. If they depend on each other, we should use sequential await.
 
     if (!post) {
@@ -57,11 +57,83 @@ export const likePost = async (req, res) => {
       success: true,
       error: false,
       message: "Post liked successfully",
+      data: {
+        postId: postId,
+        likesCount: updatedPost.likes.length,
+        isLiked: true,
+      },
     });
   } catch (error) {
     return res.status(500).json({
       success: false,
       error: true,
+      message: error.message || error,
+    });
+  }
+};
+
+export const unlikePost = async (req, res) => {
+  try {
+    //fetch userId and postId
+    const { postId } = req.body;
+    const userId = req.user.userId;
+
+    //validation
+    if (!postId || !userId) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Something went wrong while fetching details",
+      });
+    }
+
+    // post exists or not and already liked or not using promises
+    const [post, liked] = await Promise.all([
+      PostModel.findById(postId),
+      LikeModel.findOne({ post: postId, user: userId }),
+    ]);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "Post not found",
+      });
+    }
+
+    if (!liked) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "You have not liked this post",
+      });
+    }
+
+    //delete like from db and update the post
+    const [_, updatedPost]= await Promise.all([
+      LikeModel.findByIdAndDelete(liked._id),
+      PostModel.findByIdAndUpdate(
+        postId,
+        { $pull: { likes: liked._id } },
+        { new: true },
+      ),
+    ]);
+
+    //return response
+    return res.status(200).json({
+      success: true,
+      error: false,
+      message: "Post unliked successfully",
+      data: {
+        postId: postId,
+        likesCount: updatedPost.likes.length,
+        isLiked: false,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      success: false,
       message: error.message || error,
     });
   }
