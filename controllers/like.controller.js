3292fcef631a0f98fa928c896c117b1a24847a30
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-16 15:08:07 +0530

    wip- added parallel update for queries in like.post.controller(likePost)

--- controllers/like.controller.js
+++ controllers/like.controller.js
@@ -16,8 +16,13 @@ export const likePost = async (req, res) => {
       });
     }
 
-    // post exists
-    const post = await PostModel.findById(postId);
+    // post exists or not and already liked or not using promises
+    const [post, liked] = await Promise.all([
+      PostModel.findById(postId),
+      LikeModel.findOne({ post: postId, user: userId })
+    ])
+    // const post = await PostModel.findById(postId); //this will search for post first then check if liked. Using promises we can do both at same time because these operations dont depend on each other and run in parallel. If they depend on each other, we should use sequential await.
+
     if (!post) {
       return res.status(404).json({
         success: false,
@@ -26,7 +31,8 @@ export const likePost = async (req, res) => {
       });
     }
     // already liked
-    const liked = await LikeModel.findOne({ post: postId, user: userId });
+    // const liked = await LikeModel.findOne({ post: postId, user: userId }); //this will search after post is found. it means it runs sequentially so it will take more time to execute. Using promises we can do both at same time because these operations dont depend on each other and run in parallel. If they depend on each other, we should use sequential await.
+
     if (liked) {
       return res.status(400).json({
         success: false,
@@ -38,17 +44,20 @@ export const likePost = async (req, res) => {
     //like create
     const newLike = await LikeModel.create({ user: userId, post: postId });
     // update post
-    const updatedPost = await PostModel.findByIdAndUpdate(postId,{
-        $addToSet:{likes:newLike._id}
-    }, {new : true})
+    const updatedPost = await PostModel.findByIdAndUpdate(
+      postId,
+      {
+        $addToSet: { likes: newLike._id },
+      },
+      { new: true },
+    );
 
     // return response
     return res.status(200).json({
-        success:true,
-        error:false,
-        message: "Post liked successfully",
-        data: updatedPost,
-    })
+      success: true,
+      error: false,
+      message: "Post liked successfully",
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
