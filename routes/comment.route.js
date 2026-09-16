e99275aac00a0ceac9c34cf3187ec82ef075a7e9
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-16 23:46:35 +0530

    registered the new api(deleteComment) in comment.route.js

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -5,5 +5,7 @@ import { createComment } from "../controllers/comments.controller.js"
 const commentRouter = express.Router()
 
 commentRouter.post("/create-comment", loginValidation, createComment)
+commentRouter.delete("/delete-comment", loginValidation, deleteComment)
+
 
 export default commentRouter
