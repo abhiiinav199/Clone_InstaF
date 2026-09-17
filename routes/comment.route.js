3fb5c31a7c24fa7a4dc4ec033c844e49d59847cc
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-17 12:01:13 +0530

    wip- fixed import in comment.route.js

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { loginValidation } from "../middlewares/Authorization.js"
-import { createComment } from "../controllers/comments.controller.js"
+import { createComment, deleteComment } from "../controllers/comments.controller.js"
 
 const commentRouter = express.Router()
 
