ea9cb6bec7c1b9d99e4618a1a87856783d416b75
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-19 17:48:05 +0530

    wip- registered rmeoveFollower in followUnfollow.route.js. Now Feature of removing follower is officially rolled out

--- routes/followUnfollow.route.js
+++ routes/followUnfollow.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { loginValidation } from "../middlewares/Authorization.js"
-import { acceptFollowRequest, follow, rejectFollowRequest, removeFollowRequest, unfollow } from "../controllers/followUnfollow.controler.js"
+import { acceptFollowRequest, follow, rejectFollowRequest, removeFollower, removeFollowRequest, unfollow } from "../controllers/followUnfollow.controler.js"
 const followUnfollowRouter = express.Router()
 
 followUnfollowRouter.post("/follow", loginValidation, follow)
@@ -8,5 +8,6 @@ followUnfollowRouter.delete("/unfollow", loginValidation, unfollow)
 followUnfollowRouter.put("/accept-follow-request", loginValidation, acceptFollowRequest)
 followUnfollowRouter.delete("/reject-follow-request/:targetUserId", loginValidation, rejectFollowRequest)
 followUnfollowRouter.delete("/remove-follow-request/:targetUserId", loginValidation, removeFollowRequest)
+followUnfollowRouter.delete("/remove-follower/:targetUserId", loginValidation, removeFollower)
 
 export default followUnfollowRouter
