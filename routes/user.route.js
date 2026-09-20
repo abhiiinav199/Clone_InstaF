86721b70fb986c8ac80bdd71f57211eba7a2d6de
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-21 00:06:31 +0530

    wip- registered editProfileDetails api in user.routes.js, Now feature of editing profile details is officially rolled out

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { login, otpSave, resetPassword, resetPasswordOtpSend, resetPasswordOtpVerify, signUp, suggestUser } from "../controllers/auth.controller.js"
 import { loginValidation } from "../middlewares/Authorization.js"
-import { profileDetails } from "../controllers/user.controller.js"
+import { editProfileDetails, profileDetails } from "../controllers/user.controller.js"
 
 const userRouter= express.Router()
 
@@ -13,4 +13,5 @@ userRouter.post("/reset-password-otp-verify", resetPasswordOtpVerify)
 userRouter.put("/reset-password", resetPassword)
 userRouter.get("/suggest-user", loginValidation, suggestUser)
 userRouter.get("/profile-details/:userId",loginValidation,profileDetails);
+userRouter.put("/edit-profile-details",loginValidation, editProfileDetails)
 export default userRouter
