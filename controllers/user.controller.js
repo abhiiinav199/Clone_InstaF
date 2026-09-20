d52abeab1fc4c1bd9a30d81c2b61059f5c643ee1
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-21 00:00:06 +0530

    wip- made editProfileDetails api in user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,7 +19,7 @@ export const profileDetails = async (req, res) => {
         .populate("followers")
         .exec(),
 
-       PostModel.find({ user: userId })
+      PostModel.find({ user: userId })
         .populate({
           path: "likes",
           populate: {
@@ -36,23 +36,61 @@ export const profileDetails = async (req, res) => {
     ]);
 
     if (!userDetails) {
-  return res.status(404).json({
-    error: true,
-    success: false,
-    message: "User not found",
-  });
-}
-
+      return res.status(404).json({
+        error: true,
+        success: false,
+        message: "User not found",
+      });
+    }
 
     return res.status(200).json({
-        error: false,
-        success: true,
-        message: "Successfully fetched profile details",
-        data:{
-            userDetails: userDetails,
-            userAllPosts: userAllPosts
-        }
-    })
+      error: false,
+      success: true,
+      message: "Successfully fetched profile details",
+      data: {
+        userDetails: userDetails,
+        userAllPosts: userAllPosts,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      success: false,
+      message: error.message || error,
+    });
+  }
+};
+
+export const editProfileDetails = async (req, res) => {
+  try {
+    const { about, dateOfBirth } = req.body;
+
+    const userId = req.user.userId;
+    if (!userId) {
+      return res.status(400).json({
+        error: true,
+        success: false,
+        message: "Something went wrong while fetching user profile",
+      });
+    }
+    const updateFields = {};
+    if (about !== undefined) updateFields.about = about;
+    if (dateOfBirth !== undefined) updateFields.dateOfBirth = dateOfBirth;
+
+    // update userDetails
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      {$set: updateFields},
+      { new: true },
+    ).select("-password");
+
+    // return response
+    return res.status(200).json({
+      error: false,
+      success: true,
+      message: "Profile details updated successfully.",
+      data: updatedUser
+    });
   } catch (error) {
     return res.status(500).json({
       error: true,
