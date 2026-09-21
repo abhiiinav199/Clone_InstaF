1b8b5b5bb4e8bd0b5197a0aa432d40c223b36f4d
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-09-22 02:17:35 +0530

    wip- registered new api homePage in post.route.js

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { loginValidation } from "../middlewares/Authorization.js"
-import { deletePost, editPost, postUpload } from "../controllers/post.controller.js"
+import { deletePost, editPost, homePage, postUpload } from "../controllers/post.controller.js"
 import { upload } from "../middlewares/multer.js"
 
 const postRouter = express.Router()
@@ -8,5 +8,7 @@ const postRouter = express.Router()
 postRouter.post("/post-upload", loginValidation, upload.array("post", 10), postUpload)
 postRouter.post("/edit-post", loginValidation, editPost)
 postRouter.delete("/delte-post/:postId", loginValidation, deletePost)
+postRouter.get("/home", loginValidation, homePage);
+
 
 export default postRouter
